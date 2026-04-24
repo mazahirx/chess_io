2bc2b7378d50480698310d550658e72a585ac34d
Mazahir Mehdi <syedmazahir099@gmail.com>
2026-04-24 12:46:07 +0500

    socket setup and error handling

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require('express');       //required frameworks
 const socket = require('socket.io');
 const http = require('http');
 const path = require('path');
 const {Chess} = require('chess.js');
 
-const app = express();
+const app = express();      //server setup
 const server = http.createServer(app);
 const io = socket(server);
 
@@ -18,5 +18,30 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get('/', (req,res) =>{
     res.render('index');
 });
+
+io.on("connection",(uniqueSocket) =>{   //socket connection
+    console.log("connected");
+    
+    if(!players.white){
+        players.white = uniqueSocket.id;
+        uniqueSocket.emit("playerRole","W");
+    }
+    else if(!players.black){
+        players.black = uniqueSocket.id;
+        uniqueSocket.emit("playerRole","B");
+    }
+    else{
+        uniqueSocket.emit("Spectator");
+    }
+
+    uniqueSocket.on("disconnet",()=>{
+        if(uniqueSocket.id == player.white){
+            delete player.white;
+        }
+        else if(uniqueSocket.id == player.black){
+            delete player.black;
+        }
+    })
+})
 
 server.listen(3000);
